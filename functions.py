70edcbd953736839c2d53940302c0a9489a38fa5
Julie LU <luchunchun@gmail.com>
2020-12-21 17:13:02 +0100

    Adding normalizer and standardizer class.

--- functions.py
+++ functions.py
@@ -20,3 +20,38 @@ def outlier_cal(df,column):
     max_v = Q3 + 1.5*IQR
     min_v = Q1 - 1.5*IQR
     return df[(df[column] > max_v)|(df[column] < min_v)]
+
+
+# Normalizer & Standardizer Class
+
+class Normalizer_Standardizer():
+    """ The normalizer class receives a dataframe as its only input for initialization,
+    then the class generates a list of parameters, which is stored in a list {self.params}"""
+
+    def __init__(self,dataframe):
+        self.params = []
+        for i in list(dataframe.columns):
+            max_v = dataframe[i].max()
+            min_v = dataframe[i].min()
+            mean_v = dataframe[i].mean()
+            std_v = dataframe[i].std()
+            self.params.append([min_v,max_v,mean_v,std_v])
+
+
+    def normalize_data(self,x):
+        """ The function receives a data point as an input and then outputs the normalized version. """
+
+        normalized = []
+        for i,j in enumerate(x):
+            normalized_v = (j-self.params[i][0])/(self.params[i][1]-self.params[i][0])
+            normalized.append(normalized_v)
+        return normalized
+
+    def standardize_data(self,x):
+        """ The function receives a data point as an input and then outputs the standardized version. """
+
+        standardized = []
+        for i,j in enumerate(x):
+            standardized_v = (j - self.params[i][2]) / self.params[i][3]
+            standardized.append(standardized_v)
+        return standardized
